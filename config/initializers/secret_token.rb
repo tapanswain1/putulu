e5df5e08c50009cf6454755b88d16f27340488da
tapanswain1 <tapan.swain@gmail.com>
2012-05-28 20:42:14 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Putulu::Application.config.secret_token = 'a1467ab5076049c6129454b27d71244c602b698df1dd21cdf8699010b20e2a1ebfc0f321aff10f2e5611901a672a9d186884242b3a261ff2187268f0b55f2753'
